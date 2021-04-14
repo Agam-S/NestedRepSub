93df01a1253966630182bcd0aeef1f3cb32f76bd
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-14 23:21:32 +1000

    sixth

--- Ex2,T1/Ex2, Task 1.cs
+++ Ex2,T1/Ex2, Task 1.cs
@@ -6,13 +6,17 @@ namespace NestedSubTask
     {
         static void Main(string[] args)
         {
+        System.Console.WriteLine("Number of x's");
+        string X = Console.ReadLine();
+
         int counter = 0;
         int counter2 = 0;
+        int number = int.Parse(X);
 
-        while (counter < 5) {
+        while (counter < number) {
         counter2 = counter;
         
-        while (counter2 < 5) {
+        while (counter2 < number) {
         System.Console.Write("x");
         counter2++;
         
