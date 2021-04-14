7ae18f86f75eaeeedc6078784d38229ac4943c31
Agam Singh <79820189+Agam-S@users.noreply.github.com>
2021-04-14 17:56:05 +1000

    Fifth

--- Ex2,T2/Program.cs
+++ Ex2,T2/Program.cs
@@ -6,29 +6,25 @@ namespace Ex2_T2
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Give me your name!!");
-        string Name = Console.ReadLine();
-
         int counter = 0;
         int counter2 = 0;
-
-        while (counter < 10) {
-        counter2 = counter;
         
-        while (counter2 < 10) {
-        System.Console.Write(Name + " ");
-        counter2++;
-        
-        }
-        System.Console.WriteLine();
+        while (counter < 5) {
         counter++;
+        
+        while (counter2 < counter) {
+        System.Console.Write("x");
+        counter2++;
+        }
+        
+        System.Console.WriteLine();
+       
         }
 
 
-
-    }
+        }
     
-}
+    }
 }
 
 
